215318a788c87b36b083e7a7d3120e75304897b1
Roger Castañeda <rogercastanedag@gmail.com>
2023-12-12 18:15:17 -0500

    Add styles to nav and buttons

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Button from './components/Button'
 function App() {
   return (
     <div className="App">
-      <Button url="/" title="Home" />
-      <Button url="/about" title="About" />
-      <Button url="/contact" title="Contact" />
+      <nav>
+        <Button url="/" title="Home" />
+        <Button url="/about" title="About" />
+        <Button url="/contact" title="Contact" />
+      </nav>
     </div>
   );
 }
